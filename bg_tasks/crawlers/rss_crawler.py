1d6016c135a99247269cc44bd1b563e394c84bf2
Teodors Chaly <iteo.chaly@gmail.com>
2024-06-14 23:25:49 +0300

    RSS cralwer created. New rss endpoints

---
+++ bg_tasks/crawlers/rss_crawler.py
@@ -0,0 +1,69 @@
+import json
+import os
+
+from fastapi import HTTPException
+import aiofiles
+
+
+async def add_by_rss_function(url, topic):
+    if not os.path.exists("RSS_news"):
+        os.makedirs("RSS_news")
+
+    if not os.path.exists(f"RSS_news/{topic}_rss_list"):
+        os.makedirs(f"RSS_news/{topic}_rss_list")
+
+    filename = f"RSS_news/{topic}_rss_list/rss_{topic}.json"
+
+    data = {
+        "feeds": []
+    }
+
+    try:
+        async with aiofiles.open(filename, 'r') as file:
+            content = await file.read()
+            if content:
+                data = json.loads(content)
+    except FileNotFoundError:
+        pass
+
+    for feed in data["feeds"]:
+        if feed["url"] == url:
+            raise HTTPException(status_code=400, detail="This RSS feed already exists.")
+
+    new_feed = {
+        "url": url,
+        "topic": topic
+    }
+    data["feeds"].append(new_feed)
+
+    async with aiofiles.open(filename, 'w') as file:
+        await file.write(json.dumps(data, ensure_ascii=False, indent=4))
+
+    return {"message": "RSS feed added successfully"}
+
+
+async def extract_all_rss_function(topic):
+    directory = "RSS_news"
+    directory2 = f"RSS_news/{topic}_rss_list"
+
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    if not os.path.exists(directory2):
+        os.makedirs(directory2)
+
+    filename = f"{directory2}/rss_{topic}.json"
+    print(filename)
+    data = {
+        "feeds": []
+    }
+
+    try:
+        async with aiofiles.open(filename, 'r', encoding='utf-8') as file:
+            content = await file.read()
+            if content:
+                data = json.loads(content)
+                return data["feeds"]
+    except FileNotFoundError:
+        pass
+    return data["feeds"]
--- bg_tasks/router.py
+++ bg_tasks/router.py
@@ -1,11 +1,13 @@
 from fastapi import Query, APIRouter
 from bg_tasks.background_tasks import scrape
+from bg_tasks.crawlers.json_save import rss_list_saver
+from bg_tasks.crawlers.rss_crawler import *
 from languages.language_json import language_json_read
 
-router = APIRouter(tags=["Scraping"])
+router = APIRouter()
 
 
-@router.post("/scrape")
+@router.post("/scrape", tags=["Scraping function"])
 async def scraper_fun(url: str = Query(..., description="URL to scrape"),
                       topic: str = Query("crypto")):
     languages = language_json_read()
@@ -13,14 +15,21 @@ async def scraper_fun(url: str = Query(..., description="URL to scrape"),
     return result
 
 
-@router.post("/scrape_by_google_key")
-async def scraper_by_key(keyword: str = Query(..., description="Keywords..."), topic: str = Query("crypto")):
-    languages = language_json_read()
-    print(languages)
-    await crawler_google_results(keyword)
-    # result = await scrape(url, topic, languages)
-    # return result
+@router.post("/crawler_by_RSS", tags=["RSS crawler"])
+async def crawler_by_rss_or_feed(topic: str = Query(..., description="Topic")):
+    list_of_urls = await extract_all_rss_function(topic)
+    new_links_number = 0
+    for url in list_of_urls:
+        new_links_number += await rss_list_saver(url["url"], url["topic"])
+    return f"RSS scraped. New - {new_links_number}"
 
 
-async def crawler_google_results(keyword):
-    print(keyword)
+@router.post("/add_rss_url", tags=["RSS config"])
+async def add_by_rss_by_url(url: str = Query(..., description="URL to RSS"),
+                            topic: str = Query(..., description="Topic")):
+    return await add_by_rss_function(url, topic)
+
+
+@router.get("/list_of_rss", tags=["RSS config"])
+async def extract_all_rss(topic: str):
+    return await extract_all_rss_function(topic)
