dccb9ef0ffc4b656a2a72ac515f7d682ef54d973
Teodors Chaly <iteo.chaly@gmail.com>
2024-06-17 16:27:50 +0300

    RSS list test function added

--- bg_tasks/crawlers/json_save.py
+++ bg_tasks/crawlers/json_save.py
@@ -3,7 +3,7 @@ import os
 from urllib.parse import urlparse
 
 import feedparser
-# import requests
+import requests
 
 
 def rss_list_saver(url, topic):
@@ -19,7 +19,6 @@ def rss_list_saver(url, topic):
 
     if not os.path.exists(f'RSS_news/{topic}_rss_sites'):
         os.makedirs(f'RSS_news/{topic}_rss_sites')
-    #
     filename = f'RSS_news/{topic}_rss_sites/{domain}_rss.json'
     max_entries = 50
 
@@ -46,14 +45,18 @@ def rss_list_saver(url, topic):
 
     return new_links
 
-# headers = {
-#                 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36',
-#                 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-#                 'Accept-Language': 'en-US,en;q=0.9',
-#                 'Accept-Encoding': 'gzip, deflate, br',
-#                 'Connection': 'keep-alive',
-#                 'Upgrade-Insecure-Requests': '1',
-#                 'Cache-Control': 'max-age=0'
-#             }
-# list1 = rss_list_saver("https://coinbold.io/feed/", "r")
-# print(requests.get(list1[0], headers=headers))
+
+def test_rss(url):
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+        'Accept-Language': 'en-US,en;q=0.9',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Connection': 'keep-alive',
+        'Upgrade-Insecure-Requests': '1',
+        'Cache-Control': 'max-age=0'
+    }
+    list1 = rss_list_saver(url, "r")
+    print(requests.get(list1[0], headers=headers))
+
+# test_rss("https://coinbold.io/feed/")
